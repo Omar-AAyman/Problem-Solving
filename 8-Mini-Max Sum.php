018435eb18513b3db7f6adcfabedf61db78eb7b2
Omar <omaraymn411@gmail.com>
2023-01-02 23:28:04 +0200

    Mini-Max Sum

---
+++ 8-Mini-Max Sum.php
@@ -0,0 +1,85 @@
+<?php
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+// function miniMaxSum($arr) {
+    // Write your code here
+    $arr = [2, 4, 7, 9, 1];
+
+    $number = 0;
+for ($i = 0; $i < count($arr); $i++) {
+    $number = 0;
+    for ($n = 0; $n < count($arr); $n++) {
+
+        $number = $number + $arr[$n];
+    }
+    $arr2[$i] = $number - $arr[$i];
+    // print_r($arr2) ;
+    // $
+    
+
+}
+echo min($arr2)." ". max($arr2);
+
+// }
+
+// $arr_temp = rtrim(fgets(STDIN));
+
+// $arr = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+// miniMaxSum($arr);
+?>
+
+
+<!-- 
+Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+Example
+
+The minimum sum is  and the maximum sum is . The function prints
+
+16 24
+Function Description
+
+Complete the miniMaxSum function in the editor below.
+
+miniMaxSum has the following parameter(s):
+
+arr: an array of  integers
+Print
+
+Print two space-separated integers on one line: the minimum sum and the maximum sum of  of  elements.
+
+Input Format
+
+A single line of five space-separated integers.
+
+Constraints
+
+
+Output Format
+
+Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than a 32 bit integer.)
+
+Sample Input
+
+1 2 3 4 5
+Sample Output
+
+10 14
+Explanation
+
+The numbers are , , , , and . Calculate the following sums using four of the five integers:
+
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Hints: Beware of integer overflow! Use 64-bit Integer.
+
+Need help to get started? Try the Solve Me First problem -->
--- New Text Document.php
+++ New Text Document.php
@@ -1,70 +1,86 @@
 <?php
-$categories = [
-    'Id' => 1,
-    'name' => 'mobiles',
-    'subCategories' => [
-        'samsung' => [
-            (object)[
-                'id' => 50,
-                'name' => 'NOTE 10',
-                'prices' => [5000, 6000],
-                'madeIn' => ['china', 'germany'],
-                'colors' => ['primary' => 'black', 'secondaryColors' => ['red', 'blue']],
-            ], (object)[
-                'id' => 60,
-                'name' => 'S20',
-                'prices' => [7000, 8000],
-                'madeIn' => ['china', 'germany'],
-                'colors' => ['primary' => 'white', 'secondaryColors' => ['brown', 'orange']],
-            ]
-        ],
-        'apple' => [
-            (object)[
-                'id' => 70,
-                'name' => 'Iphone X',
-                'prices' => 20000,
-                'madeIn' => (object)['counrty' => 'china'],
-                'colors' => ['red', 'green', 'black'],
-            ], (object)[
-                'id' => 80,
-                'name' => 'Iphone 12',
-                'prices' => 60000,
-                'madeIn' => (object)['counrty' => 'china'],
-                'colors' => [
-                    (object)['primary' => 'black'],
-                    (object)[
-                        'secondaryColors' =>
-                        [
-                            'red',
-                            'blue'
-                        ]
-                    ]
-                ],
-            ]
-        ],
-        'oppo' => [
-            (object)[
-                'id' => 90,
-                'name' => 'Oppo F1',
-                'prices' => [],
-                'madeIn' => [],
-                'colors' => [],
-            ], (object)[
-                'id' => 100,
-                'name' => 'Oppo F2',
-                'prices' => [],
-                'madeIn' => [],
-                'colors' => [],
-            ]
-        ]
-    ]
-];
-# iphone 12 colors red , blue
+// $categories = [
+//     'Id' => 1,
+//     'name' => 'mobiles',
+//     'subCategories' => [
+//         'samsung' => [
+//             (object)[
+//                 'id' => 50,
+//                 'name' => 'NOTE 10',
+//                 'prices' => [5000, 6000],
+//                 'madeIn' => ['china', 'germany'],
+//                 'colors' => ['primary' => 'black', 'secondaryColors' => ['red', 'blue']],
+//             ], (object)[
+//                 'id' => 60,
+//                 'name' => 'S20',
+//                 'prices' => [7000, 8000],
+//                 'madeIn' => ['china', 'germany'],
+//                 'colors' => ['primary' => 'white', 'secondaryColors' => ['brown', 'orange']],
+//             ]
+//         ],
+//         'apple' => [
+//             (object)[
+//                 'id' => 70,
+//                 'name' => 'Iphone X',
+//                 'prices' => 20000,
+//                 'madeIn' => (object)['counrty' => 'china'],
+//                 'colors' => ['red', 'green', 'black'],
+//             ], (object)[
+//                 'id' => 80,
+//                 'name' => 'Iphone 12',
+//                 'prices' => 60000,
+//                 'madeIn' => (object)['counrty' => 'china'],
+//                 'colors' => [
+//                     (object)['primary' => 'black'],
+//                     (object)[
+//                         'secondaryColors' =>
+//                         [
+//                             'red',
+//                             'blue'
+//                         ]
+//                     ]
+//                 ],
+//             ]
+//         ],
+//         'oppo' => [
+//             (object)[
+//                 'id' => 90,
+//                 'name' => 'Oppo F1',
+//                 'prices' => [],
+//                 'madeIn' => [],
+//                 'colors' => [],
+//             ], (object)[
+//                 'id' => 100,
+//                 'name' => 'Oppo F2',
+//                 'prices' => [],
+//                 'madeIn' => [],
+//                 'colors' => [],
+//             ]
+//         ]
+//     ]
+// ];
+// # iphone 12 colors red , blue
 
-echo $categories['subCategories']['apple'][1]->name .
-    $categories['subCategories']['apple'][1]->colors[1]->secondaryColors[0] . ',' .
-    $categories['subCategories']['apple'][1]->colors[1]->secondaryColors[1];
-// echo $categories['subCategories']['apple'][1]->name . ' colors ' .
+// echo $categories['subCategories']['apple'][1]->name .
 //     $categories['subCategories']['apple'][1]->colors[1]->secondaryColors[0] . ',' .
 //     $categories['subCategories']['apple'][1]->colors[1]->secondaryColors[1];
-?>
+// // echo $categories['subCategories']['apple'][1]->name . ' colors ' .
+// //     $categories['subCategories']['apple'][1]->colors[1]->secondaryColors[0] . ',' .
+// //     $categories['subCategories']['apple'][1]->colors[1]->secondaryColors[1];
+
+$arr = [2, 4, 7, 9, 1];
+$number = 0;
+for ($i = 0; $i < count($arr); $i++) {
+    $number = 0;
+    for ($n = 0; $n < count($arr); $n++) {
+
+        $number = $number + $arr[$n];
+    }
+    $arr2[$i] = $number - $arr[$i];
+    // print_r($arr2) ;
+    // $
+    
+
+}
+echo max($arr2)."<br>". min($arr2);
+// echo $number;
